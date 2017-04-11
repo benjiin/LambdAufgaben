97b9ede765bb14017d009ef7b11b6eab9d78b12e
ben <nemewesa@gmail.com>
2017-04-11 13:53:50 +0200

    erste Formel

--- LambdAufgaben/Program.cs
+++ LambdAufgaben/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LambdAufgaben
+namespace LambdAufgaben    
 {
+    public delegate long potenzMaker(int a, int b);
+
     class Program
     {
+        // Potenz
+        
+        //public static int potenz(int a, int b)
+        //{
+        //    int x = a;
+        //    for(int i=1; i<=b-1; i++)
+        //    {
+        //        x = x * a;
+        //    }
+        //    return x;
+        //}
         static void Main(string[] args)
         {
+            Console.WriteLine(potenz(2, 3));
         }
     }
 }
