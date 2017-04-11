54f2c5ded6e0f60c92a118de6cddb5718633db5d
ben <nemewesa@gmail.com>
2017-04-11 14:12:41 +0200

    asda

--- LambdAufgaben/Program.cs
+++ LambdAufgaben/Program.cs
@@ -6,24 +6,29 @@ using System.Threading.Tasks;
 
 namespace LambdAufgaben    
 {
-    public delegate long potenzMaker(int a, int b);
+    public delegate long potenzMaker(int faktor, int potenz);
 
     class Program
     {
         // Potenz
-        
-        //public static int potenz(int a, int b)
+
+        //public static int potenz(int faktor, int potenz)
         //{
-        //    int x = a;
-        //    for(int i=1; i<=b-1; i++)
+        //    int x = faktor;
+        //    for (int i = 1; i <= potenz - 1; i++)
         //    {
-        //        x = x * a;
+        //        x = x * faktor;
         //    }
         //    return x;
         //}
+        //Console.WriteLine(potenz(2,3));
+
         static void Main(string[] args)
         {
-            Console.WriteLine(potenz(2, 3));
+            potenzMaker viagra;
+            viagra = (faktor, potenz) => potenz == 0 ? faktor = 1 : faktor * viagra(2, 3);
+
+            Console.WriteLine(viagra(2,3));
         }
     }
 }
